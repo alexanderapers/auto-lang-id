1744f3dc41e7fec62b5f5b488000c117f33f4f96
alexanderapers <alexander@apers.nl>
2022-05-20 19:35:02 +0200

    Added a test scenario for tokenizer

---
+++ tests/tokenize_test.py
@@ -0,0 +1,15 @@
+# Demo:
+import sys
+sys.path.append('../')
+
+from n_grams import tokenize
+
+tokens = tokenize('This is <cough,cough> "HAL-9000".  Don\'t touch!')
+print(tokens)
+
+# Test:
+if tokens == ['This', 'is', 'cough', 'cough', 'HAL-9000', "Don't", 'touch']:
+    print("ok")
+else:
+    import sys
+    print("Incorrect result", file=sys.stderr)
