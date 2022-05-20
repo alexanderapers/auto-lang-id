d2efbd4afd559d2cb8d8f7b9301776c0e1b6c938
alexanderapers <alexander@apers.nl>
2022-05-20 19:52:59 +0200

    Made a convert to ngrams function

--- n_grams.py
+++ n_grams.py
@@ -2,3 +2,8 @@ import re
 
 def tokenize(text):
     return re.sub(r"[!?\",.()<>]", " ", text).split()
+
+
+def convert_to_ngrams(sequence, n=3):
+    number_ngrams = len(sequence) - n + 1
+    return [sequence[i:i+n] for i in range(number_ngrams)]
