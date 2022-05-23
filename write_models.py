53b22eddb638bb910064e6dc31ac6985c3f37650
alexanderapers <alexander@apers.nl>
2022-05-23 22:56:46 +0200

    Completed make models from a folder of texts

--- write_models.py
+++ write_models.py
@@ -1,15 +1,22 @@
 from os.path import isdir, join
 import os
+from n_grams import frequencies, write_table
 
-def make_models(path, n, limit):
+def make_models(path_to_datafolder, n, limit):
     if not isdir("./models"):
         os.mkdir("./models")
 
     if not isdir(f"./models/{n}-{limit}"):
         os.mkdir(f"./models/{n}-{limit}")
 
+    for filename in os.listdir(path_to_datafolder):
+        language = filename.split("-")[0]
+        encoding = filename.split("-")[1]
 
-
+        with open(join(path_to_datafolder, filename), "r", encoding=encoding) as conn:
+            content = conn.read()
+            frequencies_dict = frequencies(content, n=n, limit=limit)
+            write_table(frequencies_dict, f"./models/{n}-{limit}/{language}")
 
 
 if __name__ == "__main__":
