33efcb4442d42d58b76a501787d3a9bd283d3d99
alexanderapers <alexander@apers.nl>
2022-05-23 14:00:36 +0200

    Created an ngrams frequency table

--- n_grams.py
+++ n_grams.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 def tokenize(text):
     return re.sub(r"[!?\",.()<>]", " ", text).split()
@@ -7,3 +8,13 @@ def tokenize(text):
 def convert_to_ngrams(sequence, n=3):
     number_ngrams = len(sequence) - n + 1
     return [sequence[i:i+n] for i in range(number_ngrams)]
+
+def frequencies(text, n=3, limit=0):
+    tokens = tokenize(text)
+    angled = ["<" + token + ">" for token in tokens]
+    ngrams = [convert_to_ngrams(word, n=n) for word in angled]
+    all_ngrams = [ngram for word_ngrams in ngrams for ngram in word_ngrams]
+    freq = Counter(all_ngrams)
+    return dict(freq.most_common(n=limit))
+
+
