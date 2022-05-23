b9d81ce3db7d5d8f5a6ee8eac91f3dc80adc94c3
alexanderapers <alexander@apers.nl>
2022-05-23 13:58:55 +0200

    Removed comment

--- tests/tokenize_test.py
+++ tests/tokenize_test.py
@@ -6,7 +6,6 @@ from n_grams import tokenize
 tokens = tokenize('This is <cough,cough> "HAL-9000".  Don\'t touch!')
 print(tokens)
 
-# Test:
 if tokens == ['This', 'is', 'cough', 'cough', 'HAL-9000', "Don't", 'touch']:
     print("ok")
 else:
